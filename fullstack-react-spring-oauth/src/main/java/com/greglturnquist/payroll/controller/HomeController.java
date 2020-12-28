9399b921ab6aa992709eb7bc2c363bc506d1dc7f
eder13 <simon.ranftl82@gmail.com>
2020-12-28 19:02:41 +0100

    controller method for jwt

--- fullstack-react-spring-oauth/src/main/java/com/greglturnquist/payroll/controller/HomeController.java
+++ fullstack-react-spring-oauth/src/main/java/com/greglturnquist/payroll/controller/HomeController.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.greglturnquist.payroll.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -28,10 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.Map;
 
-/**
- * @author Greg Turnquist
- */
-// tag::code[]
 @Controller // <1>
 public class HomeController {
 
@@ -55,10 +36,8 @@ public class HomeController {
 	}
 
 	@ResponseBody
-	@GetMapping("/authorized-client")
-	public String authorizedClient(@RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient) {
-		return "{ " + "\"accessToken\":"  + " \"" +  authorizedClient.getAccessToken().getTokenValue() + "\" " + "}";
+	@GetMapping("/access-token")
+	public String accessToken(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+		return "{ " + "\"accessToken\":"  + " \"" + authorizedClient.getAccessToken().getTokenValue() + "\" " + "}";
 	}
-
 }
-// end::code[]
